3d2bb9c3be8d63cfff8a927f172685f1f0db2c40
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-08-02 19:27:16 +0530

    MCP made and front and back are going

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,14 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUp,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,21 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <section className="min-h-screen w-screen flex items-center justify-center">
-            <SignedOut>
-              <SignUp />
-            </SignedOut>
-          </section>
-          <SignedIn>
-            {children}
-          </SignedIn>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
+    </html>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+const app = express();
+app.use(cors());
+
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  return res.json({ status: "All good" });
+});
+
+app.post("/upload/pdf", upload.single("pdf"), (req, res) => {
+  return res.json({ message: "uploaded" });
+});
+
+app.listen(8000, () => console.log(`Server started on PORT: ${PORT}`));
